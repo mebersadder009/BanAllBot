8b802e8ba1ba0b8ec96f7dbcc3e07226a0d2f992
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-02-02 08:43:50 +0530

    Update __main__.py

--- Code/__main__.py
+++ Code/__main__.py
@@ -11,4 +11,15 @@ from asyncio import sleep
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
 from telethon.tl.functions.channels import EditBannedRequest
 from datetime import datetime
-from var import Config as Var
+from . import Config as Var
+
+logging.basicConfig(level=logging.INFO)
+
+print("BlackMagic...Beginnings")
+
+Raichu = TelegramClient('Raichu', Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
+
+
+SUDO_USERS = []
+for x in Var.SUDO: 
+    SUDO_USERS.append(x)
